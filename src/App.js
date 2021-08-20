799e44ae4838d908d8720dfbb46ec507189be28a
shrutileena <shrutileena@gmail.com>
2021-08-20 11:51:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+
+  // BEM Naming convention
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Lets build a Whatsapp Clone!</h1>
+
+      <div className="app_body">
+        {/**/}
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC36PixJakZCuQ--LtJBhifywjHmA9hkI8",
+    authDomain: "whatsapp-clone-35df1.firebaseapp.com",
+    projectId: "whatsapp-clone-35df1",
+    storageBucket: "whatsapp-clone-35df1.appspot.com",
+    messagingSenderId: "635677554455",
+    appId: "1:635677554455:web:5d536ea5b6720fbb61a952",
+    measurementId: "G-RTVE3G5THG"
+  };
