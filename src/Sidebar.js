c937ecaf093c8dc32056904a01ad26d2f4b3ee16
shrutileena <shrutileena@gmail.com>
2021-08-25 12:28:40 +0530

    changes in sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import './Sidebar.css';
+import {Avatar, IconButton} from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-
+                <Avatar />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
             </div>
 
             <div className="sidebar__search">
-
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>  
             </div>
 
             <div className="sidebar__chats">
-
+                
             </div>
         </div>
     )
 }
 
-export default Sidebar
+export default Sidebar;
