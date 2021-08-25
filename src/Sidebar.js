2a8b3876e61fe4f7f28b62bfbc410ce1cd153aed
shrutileena <shrutileena@gmail.com>
2021-08-25 15:27:50 +0530

    changes in chat

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,7 @@ import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
@@ -32,7 +33,10 @@ function Sidebar() {
             </div>
 
             <div className="sidebar__chats">
-                
+                <SidebarChat addNewChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
             </div>
         </div>
     )
